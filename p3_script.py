558a487647160cb95464711633df96c1bfbe143e
achall1991 <75376230+achall1991@users.noreply.github.com>
2020-12-03 12:19:49 +0100

    Update p3_script.py

--- p3_script.py
+++ p3_script.py
@@ -16,7 +16,7 @@ for element in combinations:
     
     for index, row in course.iterrows(): # run through the series
         if index % incr == 0 & ~(index == 0):   # check if the row number is divisible by 'incr' and not 0 (first element is not counted for the challenge)
-            if row.iloc[0][i] == '#': # check if the element at position i
+            if row.iloc[0][i] == '#': # check if the element at position i contains a 'tree' (#)
                 t += 1              #increment t to count number of 'trees' encountered
             i = (i + inci) % 31     #increment i by number right required, then modulo 31 to 'loop' it back round to the start of the row
     element[2] = t #store number of trees enountered for each set of instructions (for error checking and check vs part 1 answer)
