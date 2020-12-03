0fc59563455a260ca8ce74e6af7c32da3a3b3713
achall1991 <75376230+achall1991@users.noreply.github.com>
2020-12-03 12:12:46 +0100

    Add files via upload

--- p3_script.py
+++ p3_script.py
@@ -4,23 +4,22 @@ import pandas as pd
 
 course = pd.read_csv('p3_input.txt', header=None)
 
-combinations = [[1, 1, 0], [3, 1, 0], [5, 1, 0], [7, 1, 0], [1, 2, 0]]
+combinations = [[1, 1, 0], [3, 1, 0], [5, 1, 0], [7, 1, 0], [1, 2, 0]] # store different ways of traversing in format plus variable for number of trees encountered for later [Right X, Down Y, Trees]
 
-i, incr, inci, t, ans = 0, 0, 0, 0, 1
+i, incr, inci, t, ans = 0, 0, 0, 0, 1 # initialise variables for the below for loop - 'ans' starting at 1 so that it can be multiplied by the number of trees for the answer
 
 for element in combinations:
-    incr = element[1]
-    inci = element[0]
+    incr = element[1] #number of rows to increment by
+    inci = element[0] #number of elements to increment i by
     
-    t = 0
-    i = 0
+    t, i = 0, 0 # reset t & i
     
-    for index, row in course.iterrows():
-        if index % incr == 0 & ~(index == 0):
-            if row.iloc[0][i] == '#':
-                t += 1
-            i = (i + inci) % 31                
-    element[2] = t
+    for index, row in course.iterrows(): # run through the series
+        if index % incr == 0 & ~(index == 0):   # check if the row number is divisible by 'incr' and not 0 (first element is not counted for the challenge)
+            if row.iloc[0][i] == '#': # check if the element at position i
+                t += 1              #increment t to count number of 'trees' encountered
+            i = (i + inci) % 31     #increment i by number right required, then modulo 31 to 'loop' it back round to the start of the row
+    element[2] = t #store number of trees enountered for each set of instructions (for error checking and check vs part 1 answer)
     
     ans *= t
 
